40a98297e891a1e58a043a5ff05b02055acff39d
Abigail <abialvarado14@unitec.edu>
2017-06-23 11:28:05 -0500

    Agregar mesas, asignar jugador y repartidor

--- main.cpp
+++ main.cpp
@@ -14,8 +14,8 @@ int main(){
 	vector <Repartidor*> Repartidores;
 	vector <Administrador*> Administradores;
 	vector <Jugador*> Jugadores;
-	int opc, rangopc, difopc;
-	string lugar, apodo, nombre, ID, rango, dificultad;
+	int opc, rangopc, difopc, opc2, num, tipopc, posjug, posrep;
+	string lugar, apodo, nombre, ID, rango, dificultad, tipo;
 	int edad, year;
 	double dineroj, sueldo;
 	
@@ -57,7 +57,7 @@ int main(){
 				cout << "Ingrese años de experiencia: ";
 				cin >> year;
 				cout << "Rango: \n1.Gerente a tiempo completo\n2.Gerente Medio tiempo\n3.Generente General: ";
-				
+				cin >> rangopc;
 				if (rangopc==1)
 					rango = "GerenteTiempoCompleto";
 				else if (rangopc==2)
@@ -89,13 +89,14 @@ int main(){
 			case 3:{
 				cout << "-------Crear Repartidor----" << endl << endl;
 				cout << "Dificultad: \n1.Dificil\n2.Intermedio\3.Facil: ";
-
+				cin >> difopc;
 				if (difopc==1)
 					dificultad = "GerenteTiempoCompleto";
 				else if (difopc==2)
 					dificultad = "GerenteMedioTiempo";
 				else
 					dificultad = "GerenteGeneral";
+
 
 				cout << "Dinero dado al casino: ";
 				cin >> dineroj;
@@ -129,15 +130,86 @@ int main(){
 				for (int i = 0; i < Jugadores.size(); ++i)
 				{
 					if (Jugadores[i]->getNombre()==nombre && Jugadores[i]->getID() == ID){
-						cout << "---BIENVENIDO A BLACKJACK!!";
+						cout << "---BIENVENIDO AL JUEGO BLACKJACK!!" << endl << endl;
 					}else{
 						if (i==Jugadores.size()-1)
-							cout << "USUARIO INCORRECTO";
+							cout << "USUARIO INCORRECTO" << endl << endl;
 					}//FIN IF, VALIDACION USUARIOS IGUALES
 					
 				}//FIN FOR, LOGIN..
 				break;
 			}//FIN CASO 4, LOGIN JUGADOR
+
+			case 5:{
+				cout << "-----LOGIN ADMINISTRADOR---" << endl << endl;
+				cout << "Ingrese nombre del administrador: ";
+				cin >> nombre;
+				cout << "Ingrese el ID del administrador: ";
+				cin >> ID;
+
+				for (int i = 0; i < Administradores.size(); ++i)
+				{
+					if (Administradores[i]->getNombre()==nombre && Administradores[i]->getID() == ID){
+						cout << "---MENU PARA ADMINISTRADORES--" << endl << endl;
+
+						cout << "Ingrese la opcion que desea:\n1.Agregar Mesa\n2.Modificar Mesa\n3.Eliminar Mesa: ";
+						cin >> opc2;
+						if (opc2==1){
+							if(Repartidores.size()==0||Jugadores.size()==0){
+								cout << "Lo sentimos, en este momento no hay disponibles jugadores ni repartidores " << endl;
+							}else{
+								//AGREGAR MESA, CON VALIDACIÓN
+								cout << "Ingrese numero de mesa: ";
+								cin >> num;
+								cout << "Ingrese tipo de mesa: \n1.VIP\n2.Clasica\n3.Viajero: ";
+								cin >> tipopc;
+
+								if (tipopc==1)
+									tipo = "VIP";
+								else if (tipopc==2)
+									tipo = "Clasica";
+								else
+									tipo = "Viajero";
+
+								cout << "--REPARTIDORES--" << endl;
+								for (int i = 0; i < Repartidores.size(); ++i)
+								{
+									cout << i << " " << Repartidores[i]->getNombre() << " " << Repartidores[i]->getDificultad() << " " << Repartidores[i]->getDinero() << endl;
+								}
+
+								cout << "Ingrese la posicion del repartidor que desea: ";
+								cin >> posrep;
+
+								while (posrep>Repartidores.size() || posrep<0){
+									cout << "ERROOR.. posicion inexistente" << endl;
+									cout << "Ingrese la posicion del repartidor que desea: ";
+									cin >> posrep;
+								}
+								cout << "---JUGADORES--" << endl;
+								for (int i = 0; i < Jugadores.size(); ++i)
+								{
+									cout << i << " " << Jugadores[i]->getNombre() << " " << Jugadores[i]->getEdad() << " " << Jugadores[i]->getDinero() << endl;
+								}
+								cout << "Ingrese la posicion del jugador que desea: ";
+								cin >> posjug;
+
+								Mesa* mes = new Mesa(num, tipo, Repartidores[posrep], Jugadores[posjug]);
+								Mesas.push_back(mes);
+
+
+
+							}//FIN AGREGAR MESAS, VALIDADAS
+
+
+						}//FIN AGREGAR MESAS, MENU
+							
+					}else{
+						if (i==Administradores.size()-1)
+							cout << "USUARIO INCORRECTO" << endl << endl;
+					}//FIN IF, VALIDACION USUARIOS IGUALES
+				}//FIN FOR.. LOGIN
+
+			}//FIN CASO 5, LOGIN ADMINISTRADOR
 		}//FIN SWITCH MENU GENERAL
 		cout << "-----BIENVENIDO A BLACK JACK-----" << endl << endl;
 		cout << "Ingrese la opción que desea: \n1.Crear Jugador\n2.Crear Administrador\n3.Crear Repartidor\n4.Login como jugador\n5.Login como Administrador:\n6.Salir ";
